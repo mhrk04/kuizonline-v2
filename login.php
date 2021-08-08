e382d3b3abbe187e02d128edd0981c8ff54dcdfc
mhrk04 <haziqrohaizan@gmail.com>
2021-08-08 18:27:43 +0800

    tambah css

--- login.php
+++ login.php
@@ -61,11 +61,13 @@ if (isset($_POST['userid'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/borang.css">
+    <link rel="stylesheet" href="/css/button.css">
     <title>Login</title>
 </head>
 <body><center>
-    <h2>Halaman Login</h2>
-    <form action="" method="post">
+    <h3 class="pendek">Halaman Login</h3>
+    <form action="" method="post" class="pendek">
         <table >
             <tr>
                 <td><label for="userid">User ID :</label></td>
