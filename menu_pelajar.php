ac863a993b2fa87e731d0188827d533893177633
mhrk04 <haziqrohaizan@gmail.com>
2021-08-09 19:14:09 +0800

    tambah css

--- menu_pelajar.php
+++ menu_pelajar.php
@@ -9,8 +9,8 @@ if ($_SESSION['status'] != "pelajar") {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
     <title>Menu Pelajar</title>
     <link rel="stylesheet" href="css/menu2.css">
 </head>
